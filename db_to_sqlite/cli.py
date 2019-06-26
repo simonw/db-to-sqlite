947b88110fe72d61b856408df7eb0e99c29ff508
Simon Willison <swillison@gmail.com>
2019-06-26 08:38:28 -0700

    Set up the foreign keys at the end, closes #1
    Also removes dependency on toposort
    
    Added --skip multi option too, refs #3

--- db_to_sqlite/cli.py
+++ db_to_sqlite/cli.py
@@ -2,7 +2,6 @@ import click
 import sqlite3
 from sqlalchemy import create_engine, inspect
 from sqlite_utils import Database
-import toposort
 
 
 @click.command()
@@ -11,9 +10,10 @@ import toposort
 @click.option("--connection", required=True, help="SQLAlchemy connection string")
 @click.option("--all", help="Detect and copy all tables", is_flag=True)
 @click.option("--table", help="Name of table to save the results (and copy)")
+@click.option("--skip", help="When using --all skip these tables", multiple=True)
 @click.option("--sql", help="Optional SQL query to run")
 @click.option("--pk", help="Optional column to use as a primary key")
-def cli(path, connection, all, table, sql, pk):
+def cli(path, connection, all, table, skip, sql, pk):
     """
     Load data from any database into SQLite.
     
@@ -21,19 +21,17 @@ def cli(path, connection, all, table, sql, pk):
     """
     if not all and not table:
         raise click.ClickException("--all OR --table required")
+    if skip and not all:
+        raise click.ClickException("--skip can only be used with --all")
     db = Database(path)
     db_conn = create_engine(connection).connect()
     if all:
         inspector = inspect(db_conn)
-        tables = toposort.toposort_flatten(
-            {
-                table: {
-                    fk["referred_table"] for fk in inspector.get_foreign_keys(table)
-                }
-                for table in inspector.get_table_names()
-            }
-        )
-        for table in tables:
+        foreign_keys_to_add = []
+        tables = inspector.get_table_names()
+        for i, table in enumerate(tables):
+            if table in skip:
+                continue
             pks = inspector.get_pk_constraint(table)["constrained_columns"]
             if len(pks) > 1:
                 click.echo("Multiple primary keys not currently supported", err=True)
@@ -42,18 +40,32 @@ def cli(path, connection, all, table, sql, pk):
             if pks:
                 pk = pks[0]
             fks = inspector.get_foreign_keys(table)
-            foreign_keys = [
-                (
-                    # column, other_table, other_column
-                    fk["constrained_columns"][0],
-                    fk["referred_table"],
-                    fk["referred_columns"][0],
-                )
-                for fk in fks
-            ]
+            foreign_keys_to_add.extend(
+                [
+                    (
+                        # table, column, other_table, other_column
+                        table,
+                        fk["constrained_columns"][0],
+                        fk["referred_table"],
+                        fk["referred_columns"][0],
+                    )
+                    for fk in fks
+                ]
+            )
             results = db_conn.execute("select * from {}".format(table))
             rows = (dict(r) for r in results)
-            db[table].upsert_all(rows, pk=pk, foreign_keys=foreign_keys)
+            db[table].upsert_all(rows, pk=pk)
+        for table, column, other_table, other_column in foreign_keys_to_add:
+            # Make sure both tables exist and are not skipped - they may not
+            # exist if they were empty and hence .upsert_all() didn't have a
+            # reason to create them
+            if (
+                db[table].exists
+                and table not in skip
+                and db[other_table].exists
+                and other_table not in skip
+            ):
+                db[table].add_foreign_key(column, other_table, other_column)
     else:
         if not sql:
             sql = "select * from {}".format(table)
