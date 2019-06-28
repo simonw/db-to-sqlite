4fbe5c97284a8a06a62377155195651208762053
Simon Willison <swillison@gmail.com>
2019-06-28 08:13:18 -0700

    Test for db-to-sqlite against two MySQL tables, refs #5

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,4 +1,8 @@
 import pytest
+from click.testing import CliRunner
+from db_to_sqlite import cli
+import sqlite_utils
+from sqlite_utils.db import ForeignKey
 
 try:
     import MySQLdb
@@ -7,27 +11,75 @@ except ImportError:
 
 
 INIT_SQL = """
-CREATE TABLE IF NOT EXISTS foo (id integer primary key);
+CREATE TABLE IF NOT EXISTS categories(
+   id int not null auto_increment primary key,
+   name varchar(32) not null
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS products(
+   id int not null auto_increment primary key,
+   name varchar(32) not null,
+   cat_id int not null,
+   FOREIGN KEY fk_cat(cat_id) REFERENCES categories(id)
+   ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+DELETE FROM products;
+DELETE FROM categories;
+
+INSERT INTO categories (id, name) VALUES (1, "Junk");
+INSERT INTO products (id, name, cat_id) VALUES (1, "Bobcat Statue", 1);
 """
 
 
 @pytest.fixture(scope="session")
-def db():
+def mysql_db():
     db = MySQLdb.connect(user="root", passwd="")
     cursor = db.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS test_db")
-    cursor.execute("USE test_db")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS test_db;")
+    cursor.execute("USE test_db;")
     cursor.execute(INIT_SQL)
+    cursor.close()
+    db.commit()
+    db.close()
+    db = MySQLdb.connect(user="root", passwd="", db="test_db")
     yield db
+    db.close()
+    db = MySQLdb.connect(user="root", passwd="")
     cursor = db.cursor()
-    cursor.execute("DROP DATABASE test_db")
+    cursor.execute("DROP DATABASE test_db;")
+    cursor.close()
+    db.commit()
     db.close()
 
 
 @pytest.mark.skipif(
     MySQLdb is None, reason="MySQLdb module not available - pip install mysqlclient"
 )
-def test_mysql(db):
-    cursor = db.cursor()
+def test_fixture(mysql_db):
+    cursor = mysql_db.cursor()
     cursor.execute("show tables")
-    assert (("foo",),) == cursor.fetchall()
+    assert (("categories",), ("products",)) == cursor.fetchall()
+
+
+@pytest.mark.skipif(
+    MySQLdb is None, reason="MySQLdb module not available - pip install mysqlclient"
+)
+def test_db_to_sqlite(mysql_db, tmpdir):
+    connection = "mysql://root@localhost/test_db"
+    db_path = str(tmpdir / "test.db")
+    result = CliRunner().invoke(cli.cli, ["--connection", connection, "--all", db_path])
+    db = sqlite_utils.Database(db_path)
+    assert {"categories", "products"} == set(db.table_names())
+    assert [{"id": 1, "name": "Bobcat Statue", "cat_id": 1}] == list(
+        db["products"].rows
+    )
+    assert [{"id": 1, "name": "Junk"}] == list(db["categories"].rows)
+    assert [
+        ForeignKey(
+            table="products",
+            column="cat_id",
+            other_table="categories",
+            other_column="id",
+        )
+    ] == db["products"].foreign_keys
