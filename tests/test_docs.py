ea1c7be5bbb3213d9ec5fb23eb207e629a1182e5
Simon Willison <swillison@gmail.com>
2019-06-29 20:35:50 -0700

    Unit test ensuring README --help stays up-to-date

---
+++ tests/test_docs.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+from db_to_sqlite.cli import cli
+from pathlib import Path
+
+readme_path = Path(__file__).parent.parent / "README.md"
+
+
+def test_readme_contains_latest_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--help"], terminal_width=88)
+    help_text = result.output
+    # The 4-space indented version of this should be in README.md
+    help_text_indented = "\n".join(
+        [
+            ("    {}".format(line) if line.strip() else "")
+            for line in help_text.split("\n")
+        ]
+    ).replace("Usage: cli ", "Usage: db-to-sqlite ")
+    readme = readme_path.read_text()
+    assert help_text_indented in readme
